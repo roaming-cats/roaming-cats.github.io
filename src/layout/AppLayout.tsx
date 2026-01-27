b70b638d5a49739308163f9c505ddd9520cb605c
Aaron Enriquez <catsoinabox39@gmail.com>
2026-01-27 22:10:31 +0800

    Update
    Change something in the app layout

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -3,8 +3,8 @@ import { GiCat } from "react-icons/gi";
 
 export default function AppLayout () {
     return (
-        <div className="flex items-start justify-start w-full h-screen bg-custom-black2">
-            <div className="lg:hidden absolute inset-0 flex items-center justify-center w-full h-16 mt-4">
+        <div className="flex w-full h-screen overflow-hidden bg-custom-black2">
+            <div className="absolute inset-0 flex items-center justify-center w-full h-16 mt-4">
                 <button className="bg-custom-beige2 font-jersey flex flex-row items-center justify-center w-44 h-11 rounded-[30px] shadow-[200px]">
                     <div className="bg-custom-black2 rounded-full h-10 w-10 flex ml-0 mr-3 items-center justify-center pr-1">
                         <GiCat className=" text-custom-beige w-7 h-7 "/>
